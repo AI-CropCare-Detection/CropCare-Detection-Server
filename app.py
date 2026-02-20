3d807344e5513a47853e453c6f463dc1aa2a3c20
Okelo123 <brianokelo3583@gmail.com>
2026-02-20 14:27:19 +0300

    merging new changes

--- app.py
+++ app.py
@@ -25,14 +25,16 @@ def check_status():
 
 # train model predictor route
 @app.post("/predict", status_code=status.HTTP_200_OK)
-def predict_disease_api(data: PredictRequest):
-    image = keras.preprocessing.image.load_img(
-        data.path_to_image  , target_size=(128, 128)
-    )
-    input_arr = keras.preprocessing.image.img_to_array(image) / 255.0
-    input_arr = np.expand_dims(input_arr, axis=0)
+@app.post("/predict")
+async def predict(file: UploadFile = File(...)):
 
-    prediction = model.predict(input_arr)
+    contents = await file.read()
+
+    image = Image.open(io.BytesIO(contents)).resize((128,128))
+    image = np.array(image) / 255.0
+    image = np.expand_dims(image, axis=0)
+
+    prediction = model.predict(image)
     result_index = np.argmax(prediction)
     confidence = float(np.max(prediction))
 
@@ -40,6 +42,22 @@ def predict_disease_api(data: PredictRequest):
         "predicted_class": class_name[result_index],
         "confidence": round(confidence, 4)
     }
+    
+# def predict_disease_api(data: PredictRequest):
+#     image = keras.preprocessing.image.load_img(
+#         data.path_to_image  , target_size=(128, 128)
+#     )
+#     input_arr = keras.preprocessing.image.img_to_array(image) / 255.0
+#     input_arr = np.expand_dims(input_arr, axis=0)
+
+#     prediction = model.predict(input_arr)
+#     result_index = np.argmax(prediction)
+#     confidence = float(np.max(prediction))
+
+#     return {
+#         "predicted_class": class_name[result_index],
+#         "confidence": round(confidence, 4)
+#     }
     
 
 
